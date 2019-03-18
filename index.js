9212e501a05fa484e9aa775884b21ca9a05b1817
macjabeth <macbethjonathan@gmail.com>
2019-03-18 20:08:11 +0000

    handle post method

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const server = express();
 server.use(express.json());
 
 // C - POST
+server.post('/api/users', (req, res) => {
+  const { body: user } = req;
+
+  if (!user.name || !user.bio) {
+    return res.status(400).json({ errorMessage: 'Please provide name and bio for the user.' });
+  }
+
+  db.insert(user).then(
+    userDocument => res.status(201).json(userDocument),
+    error => res.status(500).json({ error: `There was an error while saving the user to the database; ${error}` })
+  );
+});
 
 // R - GET
 
