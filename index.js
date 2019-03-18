e8840ac6110d4bcdb2b02dd3784cadb32ccbc493
macjabeth <macbethjonathan@gmail.com>
2019-03-18 18:51:53 +0000

    server created and comments in place

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// implement your API here
+const express = require('express');
+const db = require('./data/db');
+const server = express();
+
+server.use(express.json());
+
+// C - POST
+
+// R - GET
+
+// U - PUT
+
+// D - DELETE
